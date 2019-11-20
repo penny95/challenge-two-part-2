112e51ac4ba83c12e1e20f8e03960babc4577152
penny95 <49157799+penny95@users.noreply.github.com>
2019-11-20 13:26:42 +0300

    Second commit
    Declare the object outside the loop

--- Obj.js
+++ Obj.js
@@ -1,8 +1,8 @@
 var number;
-for(number=0; number<=15; number++)
-{
-   var obj={
-      [number]:[number]*[number],
-   }
-console.log(obj);
+var obj={};
+for(number=0; number<=15; number++){
+    obj[number]=number**2;
+    
 }
+console.log(obj);
+
